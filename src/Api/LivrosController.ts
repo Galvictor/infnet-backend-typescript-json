2976484d4eb25cebddcd5cdd22cf25c46bdb9e5e
jlopes <joao.victor@lojainterativa.com>
2025-08-13 21:01:28 -0300

    Adiciona rotas ao LivrosController para listar livros utilizando o método buscarLivros

--- src/Api/LivrosController.ts
+++ src/Api/LivrosController.ts
@@ -1,9 +1,21 @@
 import LivroRepositorio from '../Infra/LivroRepositorio';
+import { Router, Request, Response } from 'express';
 
 export default class LivrosController {
     private readonly livroRepositorio: LivroRepositorio;
+    public router: Router = Router();
 
     constructor(livroRepositorio: LivroRepositorio) {
         this.livroRepositorio = livroRepositorio;
+        this.routes();
+    }
+
+    public routes() {
+        this.router.get('/livros', this.buscarLivros.bind(this));
+    }
+
+    public buscarLivros(req: Request, res: Response) {
+        const livros = this.livroRepositorio.listarLivros();
+        res.json(livros);
     }
 }

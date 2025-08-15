288011116d2beb0479d076decc69e2c185834f50
jlopes <joao.victor@lojainterativa.com>
2025-08-14 21:21:07 -0300

    Adiciona comentários explicativos sobre os tipos CriarLivroDTO e AtualizarLivroDTO no arquivo LivroDTO.ts, detalhando suas definições utilizando Omit e Partial, respectivamente. Essa atualização melhora a compreensão do código e facilita a manutenção futura.

--- src/dtos/LivroDTO.ts
+++ src/dtos/LivroDTO.ts
@@ -2,6 +2,13 @@ import { Livro } from '../entities/Livro';
 
 export type CriarLivroDTO = Omit<Livro, 'id'>;
 
+//CriarLivroDTO com omit é igual a:
+//export type CriarLivroDTO = {
+//    titulo: string;
+//    autor: string;
+//    ano: number;
+//};
+
 export type ViewLivroDTO = {
     id: number;
     titulo: string;
@@ -10,6 +17,13 @@ export type ViewLivroDTO = {
 };
 
 export type AtualizarLivroDTO = Partial<Livro>;
+
+//AtualizarLivroDTO com partial é igual a:
+//export type AtualizarLivroDTO = {
+//    titulo?: string;
+//    autor?: string;
+//    ano?: number;
+//};
 
 // Exemplos de uso dos tipos utilitários do TypeScript:
 // export type ExemploLivroDTO = Pick<Livro, 'titulo' | 'autor' | 'ano'>;
